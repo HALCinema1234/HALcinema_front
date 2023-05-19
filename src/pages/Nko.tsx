3284b75483ba917ff2a1853e1da90e9871c75519
noncha-ts023 <2a3np.c08qt@gmail.com>
2023-05-19 12:07:30 +0900

    NEW: Nko.tsxのadd

---
+++ src/pages/Nko.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Nko = () => {
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+    return (
+        <div>
+            Nko
+            <p>aaaaaaaaa</p>
+        </div>
+    );
+};
+
+export default Nko;

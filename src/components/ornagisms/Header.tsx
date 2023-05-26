aa513345fd36a9dbd9a0df58e9c5948cc8024d3b
NewNomad <tosiki1997@gmail.com>
2023-05-26 15:14:29 +0900

    👌 IMPROVE: cssの追加

--- src/components/ornagisms/Header.tsx
+++ src/components/ornagisms/Header.tsx
@@ -1,5 +1,17 @@
+import styled from '@emotion/styled';
 import React from 'react';
 
 export const Header = () => {
-    return <header></header>;
+    return <SHeader></SHeader>;
 };
+
+const SHeader = styled.header`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 60px;
+    background-color: #fff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    z-index: 100;
+`;

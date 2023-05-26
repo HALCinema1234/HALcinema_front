66a01be70dd623dd10acff7bf8915d39d4b7232c
NewNomad <tosiki1997@gmail.com>
2023-05-26 09:35:14 +0900

    👌 IMPROVE: Home.module.cssの削除による修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import { Inter } from '@next/font/google';
-import styles from '../styles/Home.module.css';
 import { SHead } from '@/components/ornagisms/SHead';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -10,7 +7,7 @@ export default function Home() {
     return (
         <>
             <SHead title='HALシネマ' />
-            <main className={styles.main}></main>
+            <main></main>
         </>
     );
 }

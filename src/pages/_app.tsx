37d80be89e278f8ee472dc48bfd3c273ad132937
NewNomad <tosiki1997@gmail.com>
2023-05-26 16:31:19 +0900

    👌 IMPROVE: Chakraのtheme変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import '../styles/reset.css';
 import type { AppProps } from 'next/app';
 
+const theme = extendTheme({
+    fonts: {
+        heading: 'Inter',
+        body: 'Inter',
+    },
+});
+
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <Component {...pageProps} />
         </ChakraProvider>
     );

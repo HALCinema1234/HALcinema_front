2dc07ee40c4143810a944461b5fe9eb5888d5be7
NewNomad <tosiki1997@gmail.com>
2023-05-30 09:43:35 +0900

    📦 NEW: movieのダミーデータを作成

--- src/pages/movie.tsx
+++ src/pages/movie.tsx
@@ -6,14 +6,62 @@ import { MovieCard } from '@/components/ornagisms/movies/MovieCard';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Movie() {
+    const movies = [
+        {
+            id: 1,
+            title: '映画タイトル',
+            image: 'https://placehold.jp/150x150.png',
+            description: '映画の説明',
+            price: 1800,
+            time: 120,
+            rating: 4.5,
+        },
+        {
+            id: 2,
+            title: '映画タイトル',
+            image: 'https://placehold.jp/150x150.png',
+            description: '映画の説明',
+            price: 1800,
+            time: 120,
+            rating: 4.5,
+        },
+        {
+            id: 2,
+            title: '映画タイトル',
+            image: 'https://placehold.jp/150x150.png',
+            description: '映画の説明',
+            price: 1800,
+            time: 120,
+            rating: 4.5,
+        },
+        {
+            id: 2,
+            title: '映画タイトル',
+            image: 'https://placehold.jp/150x150.png',
+            description: '映画の説明',
+            price: 1800,
+            time: 120,
+            rating: 4.5,
+        },
+        {
+            id: 2,
+            title: '映画タイトル',
+            image: 'https://placehold.jp/150x150.png',
+            description: '映画の説明',
+            price: 1800,
+            time: 120,
+            rating: 4.5,
+        },
+    ];
+
+    const MoviewCardContainer = () => <div></div>;
+
     return (
         <>
             <SHead title='HALシネマ' />
             <Header />
             <main>
-                <div>
-                    <MovieCard />
-                </div>
+                <MoviewCardContainer />
             </main>
         </>
     );

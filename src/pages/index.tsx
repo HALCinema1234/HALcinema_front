2e3b50c9eadf61a448e8a29372b14cdf39ec1473
NewNomad <tosiki1997@gmail.com>
2023-06-01 10:02:50 +0900

    👌 IMPROVE: css変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Inter } from '@next/font/google';
 import { SHead } from '@/components/ornagisms/SHead';
 import { Header } from '@/components/ornagisms/Header';
 import { MainContainer } from '@/components/atoms/MainContainer';
+import styled from '@emotion/styled';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,7 +12,25 @@ export default function Home() {
             <SHead title='映画一覧' />
             <Header />
 
-            <MainContainer></MainContainer>
+            <MainContainer>
+                <S>
+                    <SCard>aaaaa</SCard>
+                    <SCard>aaaaa</SCard>
+                    <SCard>aaaaa</SCard>
+                </S>
+            </MainContainer>
         </>
     );
 }
+
+const S = styled.div`
+    display: 'grid';
+    grid-template-columns: '1fr 1fr 1fr';
+`;
+
+const SCard = styled.div`
+    width: 100;
+    height: 100;
+    border: 1px solid #999;
+    border-radius: 10px;
+`;

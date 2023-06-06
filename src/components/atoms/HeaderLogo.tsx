20d01eb8afddf7b093d0101d1d1d15eeb1c5007e
NewNomad <tosiki1997@gmail.com>
2023-06-07 07:21:09 +0900

    📦 NEW:

--- src/components/atoms/HeaderLogo.tsx
+++ src/components/atoms/HeaderLogo.tsx
@@ -1,5 +1,10 @@
+import Image from 'next/image';
 import React from 'react';
 
 export const HeaderLogo = () => {
-    return <div>Logo</div>;
+    return (
+        <div>
+            <Image src='/hclogo.png' width={200} height={50} alt='' />
+        </div>
+    );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,15 @@ export default function Home() {
             <Header />
 
             <MainContainer>
+                <Image
+                    src={'/dummy/Interstellar-HD_1920x1200.jpg'}
+                    width={1200}
+                    height={600}
+                    alt=''
+                    style={{
+                        marginTop: '100px',
+                    }}
+                />
                 <S>
                     <SCard>
                         <Image
@@ -76,6 +85,149 @@ export default function Home() {
                         </p>
                     </SCard>
                 </S>
+                <div
+                    style={{
+                        boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+                        borderRadius: '10px',
+                        padding: '20px',
+                        marginTop: '100px',
+                    }}
+                >
+                    <div
+                        style={{
+                            display: 'grid',
+                            gridTemplateColumns: '10fr 2fr',
+                        }}
+                    >
+                        <h1
+                            style={{
+                                fontSize: '50px',
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            おすすめ映画
+                        </h1>
+                        <div>
+                            一覧へ
+                            <button
+                                style={{
+                                    backgroundColor: '#0fbdff',
+                                    width: '50px',
+                                    height: '50px',
+                                    border: 'none',
+                                    color: 'white',
+                                    fontSize: '30px',
+                                    fontWeight: 'bold',
+                                    marginLeft: '10px',
+                                }}
+                            >{`>`}</button>
+                        </div>
+                    </div>
+
+                    <div
+                        style={{
+                            display: 'grid',
+                            gridTemplateColumns: 'repeat(3, 1fr)',
+                            gridGap: '20px',
+                        }}
+                    >
+                        <Image
+                            src='/dummy/Interstellar-HD_1920x1200.jpg'
+                            alt=''
+                            width={500}
+                            height={800}
+                            style={{
+                                borderRadius: '10px 10px 0 0',
+                            }}
+                        />
+                        <Image
+                            src='/dummy/Interstellar-HD_1920x1200.jpg'
+                            alt=''
+                            width={500}
+                            height={800}
+                            style={{
+                                borderRadius: '10px 10px 0 0',
+                            }}
+                        />
+                        <Image
+                            src='/dummy/Interstellar-HD_1920x1200.jpg'
+                            alt=''
+                            width={500}
+                            height={800}
+                            style={{
+                                borderRadius: '10px',
+                            }}
+                        />
+                    </div>
+
+                    <div
+                        style={{
+                            display: 'grid',
+                            gridTemplateColumns: '10fr 2fr',
+                            marginTop: '100px',
+                        }}
+                    >
+                        <h1
+                            style={{
+                                fontSize: '50px',
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            公開予定映画
+                        </h1>
+                        <div>
+                            一覧へ
+                            <button
+                                style={{
+                                    backgroundColor: '#0fbdff',
+                                    width: '50px',
+                                    height: '50px',
+                                    border: 'none',
+                                    color: 'white',
+                                    fontSize: '30px',
+                                    fontWeight: 'bold',
+                                    marginLeft: '10px',
+                                }}
+                            >{`>`}</button>
+                        </div>
+                    </div>
+
+                    <div
+                        style={{
+                            display: 'grid',
+                            gridTemplateColumns: 'repeat(3, 1fr)',
+                            gridGap: '20px',
+                        }}
+                    >
+                        <Image
+                            src='/dummy/Interstellar-HD_1920x1200.jpg'
+                            alt=''
+                            width={500}
+                            height={800}
+                            style={{
+                                borderRadius: '10px 10px 0 0',
+                            }}
+                        />
+                        <Image
+                            src='/dummy/Interstellar-HD_1920x1200.jpg'
+                            alt=''
+                            width={500}
+                            height={800}
+                            style={{
+                                borderRadius: '10px 10px 0 0',
+                            }}
+                        />
+                        <Image
+                            src='/dummy/Interstellar-HD_1920x1200.jpg'
+                            alt=''
+                            width={500}
+                            height={800}
+                            style={{
+                                borderRadius: '10px',
+                            }}
+                        />
+                    </div>
+                </div>
             </MainContainer>
         </>
     );
@@ -91,6 +243,6 @@ const S = styled.div`
 const SCard = styled.div`
     width: 100;
     height: 100;
-    border: 1px solid #999;
     border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 1);
 `;

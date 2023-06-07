97ec2cf7c9540092bc960a1a0fa0b6b37db6fb5e
NewNomad <tosiki1997@gmail.com>
2023-06-07 11:04:12 +0900

    👌 IMPROVE:

--- src/components/molecules/HeaderUser.tsx
+++ src/components/molecules/HeaderUser.tsx
@@ -1,11 +1,44 @@
 import styled from '@emotion/styled';
 import React from 'react';
 import { HeaderLogin } from '../atoms/HeaderLogin';
+import {
+    Button,
+    Input,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    useDisclosure,
+} from '@chakra-ui/react';
 
 export const HeaderUser = () => {
     const user = 1;
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-    return user ? <SHeaderUserIcon /> : <HeaderLogin />;
+    return (
+        <>
+            {user ? <SHeaderUserIcon onClick={onOpen} /> : <HeaderLogin />}
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>ログイン</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Input placeholder='ユーザー名' size='lg' variant='filled' focusBorderColor='blue.400' />
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            閉じる
+                        </Button>
+                        <Button variant='ghost'>ログイン</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    );
 };
 
 const SHeaderUserIcon = styled.div`

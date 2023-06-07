93381f6c45dbce871012588fc47ebdae79c9b50f
NewNomad <tosiki1997@gmail.com>
2023-06-07 12:48:51 +0900

    📦 NEW:

--- src/pages/theaterinf.tsx
+++ src/pages/theaterinf.tsx
@@ -1,6 +1,7 @@
 import { MainContainer } from '@/components/atoms/MainContainer';
 import { Header } from '@/components/ornagisms/Header';
 import { SHead } from '@/components/ornagisms/SHead';
+import { TheatreTable } from '@/components/ornagisms/theatreinf/TheatreTable';
 import Image from 'next/image';
 import React from 'react';
 
@@ -18,55 +19,17 @@ export default function Theaterinf() {
                     }}
                 >
                     施設情報
-                    <div
-                        style={{
-                            display: 'grid',
-                            gridTemplateColumns: '4fr 8fr',
-                        }}
-                    >
-                        <Image src='/halimage.png' width={300} height={800} alt='' />
-                        <div
-                            style={{
-                                marginLeft: '30px',
-                            }}
-                        >
-                            <p
-                                style={{
-                                    fontSize: '30px',
-                                    // fontWeight: 'bold',
-                                }}
-                            >
-                                住所
-                            </p>
-                            450-0002 <br />
-                            名古屋市中村区名駅4丁目27-1
-                            <p
-                                style={{
-                                    fontSize: '30px',
-                                }}
-                            >
-                                電話番号
-                            </p>
-                            123-456-7890
-                            <p
-                                style={{
-                                    fontSize: '30px',
-                                }}
-                            >
-                                営業時間
-                            </p>
-                            10:00~22:00
-                            <p
-                                style={{
-                                    fontSize: '30px',
-                                }}
-                            >
-                                定休日
-                            </p>
-                            なし
-                        </div>
-                    </div>
                 </h1>
+
+                <div
+                    style={{
+                        display: 'grid',
+                        gridTemplateColumns: '4fr 8fr',
+                    }}
+                >
+                    <Image src='/halimage.png' width={300} height={800} alt='' />
+                    <TheatreTable />
+                </div>
             </MainContainer>
         </>
     );

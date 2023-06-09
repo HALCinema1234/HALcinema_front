e061de5e04bbbd5a7e01051bd34c8217d4c519b5
NewNomad <tosiki1997@gmail.com>
2023-06-09 15:57:57 +0900

    📦 NEW: 各予約ページ追加

---
+++ src/pages/reserve/reserve3.tsx
@@ -0,0 +1,27 @@
+import { MainContainer } from '@/components/atoms/MainContainer';
+import { Header } from '@/components/ornagisms/Header';
+import { SHead } from '@/components/ornagisms/SHead';
+import { Paymenttable } from '@/components/ornagisms/payment/Paymenttable';
+import React from 'react';
+
+export const reserve1 = () => {
+    return (
+        <>
+            <SHead title='映画一覧' />
+            <Header />
+
+            <MainContainer>
+                <h1
+                    style={{
+                        fontSize: '50px',
+                        fontWeight: 'bold',
+                    }}
+                >
+                    チケット料金一覧
+                </h1>
+
+                <Paymenttable />
+            </MainContainer>
+        </>
+    );
+};

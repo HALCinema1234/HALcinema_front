0d4236e8c05d482e6754876ba6762912beabd6b5
NewNomad <tosiki1997@gmail.com>
2023-06-30 12:28:22 +0900

    📦 NEW:

---
+++ src/components/ornagisms/movies/SearchModal.tsx
@@ -0,0 +1,10 @@
+import { Card, Heading } from '@chakra-ui/react';
+import React from 'react';
+
+export const SearchModal = () => {
+    return (
+        <Card>
+            <Heading>映画タイトル</Heading>
+        </Card>
+    );
+};

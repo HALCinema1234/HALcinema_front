ca3fb4e4fee79fd665d47a03396ccd7336b41930
NewNomad <tosiki1997@gmail.com>
2023-07-07 09:18:08 +0900

    🐛 FIX: 型の命名規則

--- src/types/seat.ts
+++ src/types/seat.ts
@@ -2,7 +2,7 @@
 // GET seats/{上映管理ID}
 // ===============================================================
 // 映画のリストを取得
-export type Seat = {
-    row: string;    // 行
-    col: number;    // 列
+export type TSeat = {
+    row: string; // 行
+    col: number; // 列
 };

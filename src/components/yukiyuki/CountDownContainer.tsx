1d874eb8786c350d6c082a1b607b9b3af26589cb
YUKI <yukiyukiyukirarara@icloud.com>
2023-07-14 12:13:56 +0900

    カウントボタン

--- src/components/yukiyuki/CountDownContainer.tsx
+++ src/components/yukiyuki/CountDownContainer.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const CountDownContainer = () => {
-  return (
-    <div>CountDownContainer</div>
-  )
-}
+    const [count, setcount] = useState(1);
+    return (
+        <div>
+            CountDownContainer
+            <button onClick={() => setcount(count +1 )}>+</button>
+            <button onClick={() => setcount(count -1 )}>-</button>{count}
+        
+
+        </div>
+    );
+};

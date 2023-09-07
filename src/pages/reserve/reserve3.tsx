ad995ad68195d14beafaae88a17ac2b1f83efd98
NewNomad <tosiki1997@gmail.com>
2023-09-08 02:40:19 +0900

    feat: ルーティングの設定

--- src/pages/reserve/reserve3.tsx
+++ src/pages/reserve/reserve3.tsx
@@ -4,16 +4,21 @@ import { Header } from '@/components/ornagisms/Header';
 import { SHead } from '@/components/ornagisms/SHead';
 import { Paymenttable } from '@/components/ornagisms/payment/Paymenttable';
 import { ReserveStepper } from '@/components/ornagisms/reserve/ReserveStepper';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 export default function reserve3() {
+    const router = useRouter();
+    const handleRoute = () => {
+        router.push('reserve4');
+    };
     return (
         <>
             <SHead title='映画一覧' />
             <Header />
             <MainContainer>
                 <ReserveStepper index={2} />
-                <DetailCard onClick={() => {}} />
+                <DetailCard onClick={handleRoute} />
             </MainContainer>
         </>
     );

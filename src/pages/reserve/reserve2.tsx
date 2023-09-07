01818a318d6bc1547af2cad2df4e61ff45b07b64
NewNomad <tosiki1997@gmail.com>
2023-09-08 01:06:00 +0900

    feat: お支払い方法選択からチケット予約へ変更

--- src/pages/reserve/reserve2.tsx
+++ src/pages/reserve/reserve2.tsx
@@ -2,25 +2,34 @@ import { MainContainer } from '@/components/atoms/MainContainer';
 import { Header } from '@/components/ornagisms/Header';
 import { SHead } from '@/components/ornagisms/SHead';
 import { Paymenttable } from '@/components/ornagisms/payment/Paymenttable';
+import { ReserveContainer } from '@/components/ornagisms/reserve/ReserveContainer';
 import { ReserveStepper } from '@/components/ornagisms/reserve/ReserveStepper';
 import { Button, Flex } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 export default function reserve2() {
+    const router = useRouter();
+
+    const handleRoute = () => {
+        router.push('reserve2');
+    };
     return (
         <>
             <SHead title='映画一覧' />
             <Header />
             <MainContainer>
                 <ReserveStepper index={1} />
-                <Flex direction='column' alignItems='center'>
-                    <Button colorScheme='cyan' padding={7} width={500} marginBottom={5}>
-                        クレジットカード
-                    </Button>
-                    <Button colorScheme='cyan' padding={7} width={500}>
-                        PayPay
-                    </Button>
-                </Flex>
+                <ReserveContainer onClick={handleRoute}>
+                    <Flex direction='column' alignItems='center'>
+                        <Button colorScheme='cyan' padding={7} width={500} marginBottom={5}>
+                            クレジットカード
+                        </Button>
+                        <Button colorScheme='cyan' padding={7} width={500}>
+                            PayPay
+                        </Button>
+                    </Flex>
+                </ReserveContainer>
             </MainContainer>
         </>
     );

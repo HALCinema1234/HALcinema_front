58d20b05276901671b75b0ce27483e7e46f38fcf
aaaa <pocchaman0393@gmail.com>
2023-09-07 09:46:20 +0900

    IMPROVE: signup画面のフォーム幅短縮

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -3,6 +3,7 @@ import { Header } from '@/components/ornagisms/Header';
 import { SHead } from '@/components/ornagisms/SHead';
 import {
     Button,
+    Flex,
     FormControl,
     FormLabel,
     Icon,
@@ -21,58 +22,60 @@ export default function signup() {
             <Header />
 
             <MainContainer>
-                <Stack spacing={4}>
-                    <FormControl isRequired>
-                        <FormLabel>メールアドレス</FormLabel>
-                        <InputGroup>
-                            <Input type='email' placeholder='Email' />
-                        </InputGroup>
-                    </FormControl>
+                <Flex direction='column' alignItems='center'>
+                    <Stack spacing={4} width={650}>
+                        <FormControl isRequired>
+                            <FormLabel>メールアドレス</FormLabel>
+                            <InputGroup>
+                                <Input type='email' placeholder='Email' />
+                            </InputGroup>
+                        </FormControl>
 
-                    <FormControl isRequired>
-                        <FormLabel>電話番号</FormLabel>
-                        <InputGroup>
-                            <Input type='tel' placeholder='000-0000-0000' />
-                        </InputGroup>
-                    </FormControl>
+                        <FormControl isRequired>
+                            <FormLabel>電話番号</FormLabel>
+                            <InputGroup>
+                                <Input type='tel' placeholder='000-0000-0000' />
+                            </InputGroup>
+                        </FormControl>
 
-                    <FormControl isRequired>
-                        <FormLabel>パスワード</FormLabel>
-                        <InputGroup>
-                            <Input type='password' placeholder='6文字以上200文字以下' />
-                        </InputGroup>
-                    </FormControl>
+                        <FormControl isRequired>
+                            <FormLabel>パスワード</FormLabel>
+                            <InputGroup>
+                                <Input type='password' placeholder='6文字以上200文字以下' />
+                            </InputGroup>
+                        </FormControl>
 
-                    <FormControl isRequired>
-                        <FormLabel>パスワード再入力</FormLabel>
-                        <InputGroup>
-                            <Input type='password' placeholder='上記のパスワードをもう一度記載' />
-                        </InputGroup>
-                    </FormControl>
+                        <FormControl isRequired>
+                            <FormLabel>パスワード再入力</FormLabel>
+                            <InputGroup>
+                                <Input type='password' placeholder='上記のパスワードをもう一度記載' />
+                            </InputGroup>
+                        </FormControl>
 
-                    <FormControl>
-                        <FormLabel>生年月日</FormLabel>
-                        <InputGroup>
-                            <Input type='datetime-local' placeholder='2000/01/01' />
-                        </InputGroup>
-                    </FormControl>
+                        <FormControl>
+                            <FormLabel>生年月日</FormLabel>
+                            <InputGroup>
+                                <Input type='datetime-local' placeholder='2000/01/01' />
+                            </InputGroup>
+                        </FormControl>
 
-                    <Button
-                        colorScheme='blue'
-                        onClick={() =>
-                            toast({
-                                position: 'bottom-left',
-                                title: 'アカウントを登録しました',
-                                description: 'ログインしてください',
-                                status: 'success',
-                                duration: 5000,
-                                isClosable: true,
-                            })
-                        }
-                    >
-                        登録
-                    </Button>
-                </Stack>
+                        <Button
+                            colorScheme='blue'
+                            onClick={() =>
+                                toast({
+                                    position: 'bottom-left',
+                                    title: 'アカウントを登録しました',
+                                    description: 'ログインしてください',
+                                    status: 'success',
+                                    duration: 5000,
+                                    isClosable: true,
+                                })
+                            }
+                        >
+                            登録
+                        </Button>
+                    </Stack>
+                </Flex>
             </MainContainer>
         </>
     );

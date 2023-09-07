bcdc764df107780db2340f834f544a9127dc9678
NewNomad <tosiki1997@gmail.com>
2023-09-08 00:35:44 +0900

    feat: シートの管理をRecoilで行うため

--- src/pages/reserve/reserve1.tsx
+++ src/pages/reserve/reserve1.tsx
@@ -4,13 +4,17 @@ import { SHead } from '@/components/ornagisms/SHead';
 import { ReserveContainer } from '@/components/ornagisms/reserve/ReserveContainer';
 import { ReserveStepper } from '@/components/ornagisms/reserve/ReserveStepper';
 import { SeatsContainerS } from '@/components/ornagisms/reserve/SeatsContainerS';
+import { reserveState } from '@/recoil/states';
 import { TSeat } from '@/types/seat';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { useRecoilState } from 'recoil';
 
 export default function reserve1() {
     const router = useRouter();
     const [seats, setSeat] = React.useState<TSeat[]>([]); // 座席の一覧と状態を管理
+
+    const [reserveInfo, setReserveInfo] = useRecoilState(reserveState);
 
     const handleRoute = () => {
         router.push('reserve2');
@@ -18,10 +22,15 @@ export default function reserve1() {
 
     // 座席をクリックしたときの処理
     const handleClickSeat = (seat: TSeat) => {
-        seats.some((s) => s.row === seat.row && s.col === seat.col)
-            ? setSeat((prev) => prev.filter((s) => s.row !== seat.row || s.col !== seat.col)) // 座席を選択したら、座席の一覧から削除
-            : setSeat((prev) => [...prev, seat]); // 座席を選択したら、座席の一覧に追加
-        console.log(seats);
+        reserveInfo.seats.some((s) => s.row === seat.row && s.col === seat.col)
+            ? setReserveInfo({
+                  ...reserveInfo,
+                  seats: reserveInfo.seats.filter((s) => s.row !== seat.row || s.col !== seat.col),
+              })
+            : setReserveInfo({
+                  ...reserveInfo,
+                  seats: [...reserveInfo.seats, seat],
+              });
     };
 
     return (
@@ -31,7 +40,7 @@ export default function reserve1() {
             <MainContainer>
                 <ReserveStepper index={0} />
                 <ReserveContainer onClick={handleRoute}>
-                    <SeatsContainerS onClick={(seat) => handleClickSeat(seat)} seats={seats} />
+                    <SeatsContainerS onClick={(seat) => handleClickSeat(seat)} seats={reserveInfo.seats} />
                 </ReserveContainer>
             </MainContainer>
         </>

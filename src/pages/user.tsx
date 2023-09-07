cd3e73e655fa47f56f5026c3f9abbb109555f6e7
NewNomad <tosiki1997@gmail.com>
2023-09-08 06:28:19 +0900

    feat: ユーザーデータ表示

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -3,16 +3,45 @@ import PageTitle from '@/components/atoms/PageTitle';
 import { Header } from '@/components/ornagisms/Header';
 import { SHead } from '@/components/ornagisms/SHead';
 import { Paymenttable } from '@/components/ornagisms/payment/Paymenttable';
-import { Flex } from '@chakra-ui/react';
-import React from 'react';
+import { userState } from '@/recoil/states';
+import { Card, CardBody, CardHeader, Flex, Text } from '@chakra-ui/react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
 
 export default function User() {
+    const user = useRecoilValue(userState);
+
+    const [data, setData] = useState();
+
+    const fetchManageData = async () => {
+        const data = await axios.get(process.env.NEXT_PUBLIC_API_BASE_URL! + 'v1/reserves/' + user.id, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        });
+        setData(data.data);
+    };
+
+    useEffect(() => {
+        (async () => {
+            await fetchManageData();
+        })();
+    }, []);
+
     return (
         <>
             <SHead title='ユーザー情報' />
             <Header />
 
-            <MainContainer></MainContainer>
+            <MainContainer>
+                <Card>
+                    <CardHeader>ユーザー情報</CardHeader>
+                    <CardBody>
+                        <Text>{data.date}</Text>
+                    </CardBody>
+                </Card>
+            </MainContainer>
         </>
     );
 }
